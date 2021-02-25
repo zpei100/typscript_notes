bd1578a354ce4603845cc1b76871c7fe59db910b
zhengqp <zpei100@gmail.com>
2021-02-25 18:34:00 -0500

    initial

---
+++ src/homework.ts
@@ -0,0 +1,21 @@
+export {}
+
+// // https://leetcode.com/problems/find-the-highest-altitude/
+// let largestAltitude = function(gain: number[]): number {
+
+// }
+
+// // https://leetcode.com/problems/count-of-matches-in-tournament/
+// let numberOfMatches = function(n: number): number {
+
+// }
+
+// // https://leetcode.com/problems/split-a-string-in-balanced-strings/
+// let balancedStringSplit = function(s: string): number {
+
+// }
+
+// // https://leetcode.com/problems/sum-of-unique-elements/
+// let sumOfUnique = function(nums: number[]): number {
+    
+// }
